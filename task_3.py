2b83537184d4c3e63167bb1f6d7a7ffa83ae91bf
antonshe84 <antonshe84@gmail.com>
2021-02-11 10:54:30 +0300

    Третье задание

--- task_3.py
+++ task_3.py
@@ -4,3 +4,11 @@
 задаем число 2 — получаем «2 процента». Вывести все склонения для проверки.
 
 """
+
+for number in range(1, 21):
+    if number == 1:
+        print(number, 'процент')
+    elif 2 <= number <= 4:
+        print(number, 'процента')
+    elif 5 <= number <= 20:
+        print(number, 'процентов')
